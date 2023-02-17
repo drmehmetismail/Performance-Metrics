de201457e730b4cf3b8900f86859f992757d89d1
drmehmetismail <125666501+drmehmetismail@users.noreply.github.com>
2023-02-17 20:57:18 +0000

    Update GPL.py

--- GPL.py
+++ GPL.py
@@ -1,4 +1,3 @@
-import chess
 import chess.pgn
 import chess.engine
 from chess.engine import Cp, Mate, MateGiven
@@ -16,8 +15,8 @@ t = 1
 n = 1000
 
 # Initialize counters for white and black's total centipawn loss
-w_tel = 0
-b_tel = 0
+w_gpl = 0
+b_gpl = 0
 
 # Initialize counters for white and black's total moves
 w_moves = 0
@@ -41,38 +40,38 @@ while True:
         else:
             b_exp = b_info['score'].black().wdl().expectation()
         board.push(move)
-        # Update the TEL and move counters: Info after the move
+        # Update the GPL and move counters: Info after the move
         a_info = engine.analyse(board, chess.engine.Limit(time=t))
         if board.turn == chess.BLACK:
-            w_tel += b_exp-a_info['score'].white().wdl().expectation()
+            w_gpl += b_exp-a_info['score'].white().wdl().expectation()
             w_moves += 1
         else:
-            b_tel += b_exp-a_info['score'].black().wdl().expectation()
+            b_gpl += b_exp-a_info['score'].black().wdl().expectation()
             b_moves += 1
             
 # Calculate the average centipawn loss for each player
-w_atel = w_tel / w_moves
-b_atel = b_tel / b_moves
+w_agpl = w_gpl / w_moves
+b_agpl = b_gpl / b_moves
 
     
-# Add the TEL headers in the game
+# Add the GPL headers in the game
 with open('carlsen-wang.pgn') as f:
     game = chess.pgn.read_game(f)
-game.headers["TEL_W"] = f": {w_tel:.2f}"
-game.headers["TEL_B"] = f": {b_tel:.2f}"
-game.headers["ATEL_W"] = f": {w_atel:.2f}"
-game.headers["ATEL_B"] = f": {b_atel:.2f}"
+game.headers["GPL_W"] = f": {w_gpl:.2f}"
+game.headers["GPL_B"] = f": {b_gpl:.2f}"
+game.headers["AGPL_W"] = f": {w_agpl:.2f}"
+game.headers["AGPL_B"] = f": {b_agpl:.2f}"
 
 # Print the results
-print("White TEL:", w_tel)
-print("Black TEL:", b_tel)
+print("White GPL:", w_gpl)
+print("Black GPL:", b_gpl)
 
-print("White ATEL:", w_atel)
-print("Black ATEL:", b_atel)
+print("White GPL:", w_agpl)
+print("Black GPL:", b_agpl)
 
-# Save the PGN file with the TELs as headers
+# Save the PGN file with the GPL as headers
 print(game)
-with open("carlsen-wang_tel.pgn", "a") as fgame:
+with open("carlsen-wang_gpl.pgn", "a") as fgame:
  print(game, file=fgame)
 
 # Close the engine
